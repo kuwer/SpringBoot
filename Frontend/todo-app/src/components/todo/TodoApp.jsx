f035eaa0c9d4f61dac96a224200dbc686828099c
kuwer_bhendarkar <bhendarkarkuwer@gmail.com>
2024-04-05 11:03:17 +0530

    Adding error component

--- Frontend/todo-app/src/components/todo/TodoApp.jsx
+++ Frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -31,6 +31,9 @@ function ErrorComponent(){
     return (
         <div className="ErrorComponent">
             <h1>Sorry the page you are looking for is broken !</h1>
+            <div>
+                Please reach out to team for more details on abc-def-ghi.
+            </div>
         </div>
     )
 }
